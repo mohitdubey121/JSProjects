9e9153e2c64e281d1c4f63e2ce596aa686979bde
mohitdubey121 <mohitdubey036@gmail.com>
2020-07-21 12:21:37 +0530

    adding incomplete webpack project

---
+++ webpack project/src/container/Pizza.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PizzaImage from '../component/pizzaImage/PizzaImage'
+class Pizza extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The Pizza</h1>
+                <PizzaImage/>
+            </div>
+        )
+    }
+}
+export default Pizza
